99938c93dcac5b761b43f59c05d078261e7627b8
wzgiceman <wzgiceman@gmail.com>
2016-12-28 16:53:31 +0800

    优化注释和缓存处理

---
+++ app/src/main/java/com/example/retrofit/entity/resulte/BaseResultEntity.java
@@ -0,0 +1,39 @@
+package com.example.retrofit.entity.resulte;
+
+/**
+ * 回调信息统一封装类
+ * Created by WZG on 2016/7/16.
+ */
+public class BaseResultEntity<T>{
+    //  判断标示
+    private int ret;
+    //    提示信息
+    private String msg;
+    //显示数据（用户需要关心的数据）
+    private T data;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+
+    public int getRet() {
+        return ret;
+    }
+
+    public void setRet(int ret) {
+        this.ret = ret;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
