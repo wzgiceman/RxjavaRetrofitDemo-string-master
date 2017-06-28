2ab7a80471a00f206c8c131ac033fc4abd50b171
wzgiceman <wzgiceman@gmail.com>
2017-06-28 15:17:17 +0800

    修改下载写入速度，避免下载失败

--- app/src/main/java/com/example/retrofit/activity/DownLaodActivity.java
+++ app/src/main/java/com/example/retrofit/activity/DownLaodActivity.java
@@ -11,6 +11,7 @@ import com.jude.easyrecyclerview.EasyRecyclerView;
 import com.wzgiceman.rxretrofitlibrary.retrofit_rx.downlaod.DownInfo;
 import com.wzgiceman.rxretrofitlibrary.retrofit_rx.downlaod.DownState;
 import com.wzgiceman.rxretrofitlibrary.retrofit_rx.utils.DbDwonUtil;
+
 import java.io.File;
 import java.util.List;
 
@@ -20,6 +21,7 @@ import java.util.List;
 public class DownLaodActivity extends AppCompatActivity {
     List<DownInfo> listData;
     DbDwonUtil dbUtil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,30 +31,34 @@ public class DownLaodActivity extends AppCompatActivity {
     }
 
     /*数据*/
-    private void initResource(){
-        dbUtil= DbDwonUtil.getInstance();
-        listData=dbUtil.queryDownAll();
+    private void initResource() {
+        dbUtil = DbDwonUtil.getInstance();
+        listData = dbUtil.queryDownAll();
         /*第一次模拟服务器返回数据掺入到数据库中*/
-        if(listData.isEmpty()){
-            String[] downUrl=new String[]{"http://www.izaodao.com/app/izaodao_app.apk",
-                    "http://download.fir.im/v2/app/install/572eec6fe75e2d7a05000008?download_token=572bcb03dad2eed7c758670fd23b5ac4"};
+        if (listData.isEmpty()) {
+            String[] downUrl = new String[]{"https://pic.newbanker" +
+                    ".cn/1491537974620__%E3%80%90%E5%9F%BA%E9%87%91%E5%90%88%E5%90%8C%E3%80%91%E5%8C%97%E4%BA%AC%E6%81%92%E5%AE" +
+                    "%87%E5%A4%A9%E6%B3%BD%E6%8A%95%E8%B5%84%E7%AE%A1%E7%90%86%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8-%E6%81%92%E5" +
+                    "%AE%87%E5%A4%A9%E6%B3%BD%E4%BA%9A%E9%A9%AC%E9%80%8A%E5%85%AB%E5%8F%B7%E7%A7%81%E5%8B%9F%E6%8A%95%E8%B5%84" +
+                    "%E5%9F%BA%E9%87%91%EF%BC%88%E4%BA%8C%E6%9C%9F%EF%BC%89.pdf", "http://www.izaodao.com/app/izaodao_app.apk",
+                    "http://www.izaodao.com/app/izaodao_app.apk", "http://www.izaodao.com/app/izaodao_app.apk"};
             for (int i = 0; i < downUrl.length; i++) {
                 File outputFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                        "test"+i + ".apk");
-                DownInfo apkApi=new DownInfo(downUrl[i]);
+                        "test" + i + (i==0?".pdf":".apk"));
+                DownInfo apkApi = new DownInfo(downUrl[i]);
                 apkApi.setId(i);
                 apkApi.setState(DownState.START);
                 apkApi.setSavePath(outputFile.getAbsolutePath());
                 dbUtil.save(apkApi);
             }
-            listData=dbUtil.queryDownAll();
+            listData = dbUtil.queryDownAll();
         }
     }
 
     /*加载控件*/
-    private void initWidget(){
-        EasyRecyclerView recyclerView=(EasyRecyclerView)findViewById(R.id.rv);
-        DownAdapter adapter=new DownAdapter(this);
+    private void initWidget() {
+        EasyRecyclerView recyclerView = (EasyRecyclerView) findViewById(R.id.rv);
+        DownAdapter adapter = new DownAdapter(this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
         adapter.addAll(listData);
--- rxretrofitlibrary/src/main/java/com/wzgiceman/rxretrofitlibrary/retrofit_rx/downlaod/HttpDownManager.java
+++ rxretrofitlibrary/src/main/java/com/wzgiceman/rxretrofitlibrary/retrofit_rx/downlaod/HttpDownManager.java
@@ -8,6 +8,10 @@ import com.wzgiceman.rxretrofitlibrary.retrofit_rx.utils.DbDwonUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -23,7 +27,6 @@ import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 import static com.wzgiceman.rxretrofitlibrary.retrofit_rx.utils.AppUtil.getBasUrl;
-import static com.wzgiceman.rxretrofitlibrary.retrofit_rx.utils.AppUtil.writeCache;
 
 /**
  * http下载处理类
@@ -33,20 +36,21 @@ public class HttpDownManager {
     /*记录下载数据*/
     private Set<DownInfo> downInfos;
     /*回调sub队列*/
-    private HashMap<String,ProgressDownSubscriber> subMap;
+    private HashMap<String, ProgressDownSubscriber> subMap;
     /*单利对象*/
     private volatile static HttpDownManager INSTANCE;
     /*数据库类*/
     private DbDwonUtil db;
 
-    private HttpDownManager(){
-        downInfos=new HashSet<>();
-        subMap=new HashMap<>();
-        db= DbDwonUtil.getInstance();
+    private HttpDownManager() {
+        downInfos = new HashSet<>();
+        subMap = new HashMap<>();
+        db = DbDwonUtil.getInstance();
     }
 
     /**
      * 获取单例
+     *
      * @return
      */
     public static HttpDownManager getInstance() {
@@ -64,21 +68,21 @@ public class HttpDownManager {
     /**
      * 开始下载
      */
-    public void startDown(final DownInfo info){
+    public void startDown(final DownInfo info) {
         /*正在下载不处理*/
-        if(info==null||subMap.get(info.getUrl())!=null){
+        if (info == null || subMap.get(info.getUrl()) != null) {
             subMap.get(info.getUrl()).setDownInfo(info);
             return;
         }
         /*添加回调处理类*/
-        ProgressDownSubscriber subscriber=new ProgressDownSubscriber(info);
+        ProgressDownSubscriber subscriber = new ProgressDownSubscriber(info);
         /*记录回调sub*/
-        subMap.put(info.getUrl(),subscriber);
+        subMap.put(info.getUrl(), subscriber);
         /*获取service，多次请求公用一个sercie*/
         HttpDownService httpService;
-        if(downInfos.contains(info)){
-            httpService=info.getService();
-        }else{
+        if (downInfos.contains(info)) {
+            httpService = info.getService();
+        } else {
             DownloadInterceptor interceptor = new DownloadInterceptor(subscriber);
             OkHttpClient.Builder builder = new OkHttpClient.Builder();
             //手动创建一个OkHttpClient并设置超时时间
@@ -91,12 +95,12 @@ public class HttpDownManager {
                     .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .baseUrl(getBasUrl(info.getUrl()))
                     .build();
-            httpService= retrofit.create(HttpDownService.class);
+            httpService = retrofit.create(HttpDownService.class);
             info.setService(httpService);
             downInfos.add(info);
         }
         /*得到rx对象-上一次下載的位置開始下載*/
-        httpService.download("bytes=" + info.getReadLength() + "-",info.getUrl())
+        httpService.download("bytes=" + info.getReadLength() + "-", info.getUrl())
                 /*指定线程*/
                 .subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.io())
@@ -106,12 +110,7 @@ public class HttpDownManager {
                 .map(new Func1<ResponseBody, DownInfo>() {
                     @Override
                     public DownInfo call(ResponseBody responseBody) {
-                        try {
-                            writeCache(responseBody,new File(info.getSavePath()),info);
-                        } catch (IOException e) {
-                            /*失败抛出异常*/
-                            throw new HttpTimeException(e.getMessage());
-                        }
+                        writeCaches(responseBody, new File(info.getSavePath()), info);
                         return info;
                     }
                 })
@@ -126,12 +125,12 @@ public class HttpDownManager {
     /**
      * 停止下载
      */
-    public void stopDown(DownInfo info){
-        if(info==null)return;
+    public void stopDown(DownInfo info) {
+        if (info == null) return;
         info.setState(DownState.STOP);
         info.getListener().onStop();
-        if(subMap.containsKey(info.getUrl())) {
-            ProgressDownSubscriber subscriber=subMap.get(info.getUrl());
+        if (subMap.containsKey(info.getUrl())) {
+            ProgressDownSubscriber subscriber = subMap.get(info.getUrl());
             subscriber.unsubscribe();
             subMap.remove(info.getUrl());
         }
@@ -142,14 +141,15 @@ public class HttpDownManager {
 
     /**
      * 暂停下载
+     *
      * @param info
      */
-    public void pause(DownInfo info){
-        if(info==null)return;
+    public void pause(DownInfo info) {
+        if (info == null) return;
         info.setState(DownState.PAUSE);
         info.getListener().onPuase();
-        if(subMap.containsKey(info.getUrl())){
-            ProgressDownSubscriber subscriber=subMap.get(info.getUrl());
+        if (subMap.containsKey(info.getUrl())) {
+            ProgressDownSubscriber subscriber = subMap.get(info.getUrl());
             subscriber.unsubscribe();
             subMap.remove(info.getUrl());
         }
@@ -160,7 +160,7 @@ public class HttpDownManager {
     /**
      * 停止全部下载
      */
-    public void stopAllDown(){
+    public void stopAllDown() {
         for (DownInfo downInfo : downInfos) {
             stopDown(downInfo);
         }
@@ -171,7 +171,7 @@ public class HttpDownManager {
     /**
      * 暂停全部下载
      */
-    public void pauseAll(){
+    public void pauseAll() {
         for (DownInfo downInfo : downInfos) {
             pause(downInfo);
         }
@@ -182,6 +182,7 @@ public class HttpDownManager {
 
     /**
      * 返回全部正在下载的数据
+     *
      * @return
      */
     public Set<DownInfo> getDownInfos() {
@@ -190,11 +191,59 @@ public class HttpDownManager {
 
     /**
      * 移除下载数据
+     *
      * @param info
      */
-    public void remove(DownInfo info){
+    public void remove(DownInfo info) {
         subMap.remove(info.getUrl());
         downInfos.remove(info);
+    }
+
+
+    /**
+     * 写入文件
+     *
+     * @param file
+     * @param info
+     * @throws IOException
+     */
+    public void writeCaches(ResponseBody responseBody, File file, DownInfo info) {
+        try {
+            RandomAccessFile randomAccessFile = null;
+            FileChannel channelOut = null;
+            InputStream inputStream = null;
+            try {
+                if (!file.getParentFile().exists())
+                    file.getParentFile().mkdirs();
+                long allLength = 0 == info.getCountLength() ? responseBody.contentLength() : info.getReadLength() + responseBody
+                        .contentLength();
+
+                inputStream = responseBody.byteStream();
+                randomAccessFile = new RandomAccessFile(file, "rwd");
+                channelOut = randomAccessFile.getChannel();
+                MappedByteBuffer mappedBuffer = channelOut.map(FileChannel.MapMode.READ_WRITE,
+                        info.getReadLength(), allLength - info.getReadLength());
+                byte[] buffer = new byte[1024 * 4];
+                int len;
+                while ((len = inputStream.read(buffer)) != -1) {
+                    mappedBuffer.put(buffer, 0, len);
+                }
+            } catch (IOException e) {
+                throw new HttpTimeException(e.getMessage());
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (channelOut != null) {
+                    channelOut.close();
+                }
+                if (randomAccessFile != null) {
+                    randomAccessFile.close();
+                }
+            }
+        } catch (IOException e) {
+            throw new HttpTimeException(e.getMessage());
+        }
     }
 
 }
